a4498b464a7ed96d1d15a018f2042e8613c838fe
ysq <ysq@HUAWEI-MateBook-X.local>
2025-06-29 09:45:00 +0800

    Initial commit

---
+++ src/main/java/itysq/jdk17jenkins/Jdk17jenkinsApplication.java
@@ -0,0 +1,13 @@
+package itysq.jdk17jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdk17jenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jdk17jenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/itysq/jdk17jenkins/Jdk17jenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package itysq.jdk17jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdk17jenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
